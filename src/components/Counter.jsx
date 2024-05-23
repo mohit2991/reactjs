6c45f7b5115e6c1708345523dd703fd870d8ad9f
Mohit Chauhan <mohit2991kumar@gmail.com>
2024-05-23 22:47:13 +0530

    Callback to pass state data child to parent

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,51 +1,18 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState } from "react";
 import CounterPrint from "./CounterPrint";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-const Counter = (Props) => {
-  // const { value } = Props;
-  // State/Props- Hook
-  const [number, setNumber] = useState(0);
-  // const [array, setArray] = useState([]);
+const Counter = () => {
+  const [initvalue, setInitvalue] = useState(10);
 
-  // useEffect(() => {
-  //   console.log(">>>>> mohit componentDidMount()");
-  // }, []);
-
-  // useEffect(() => {
-  //   if (number !== 0) {
-  //     console.log(">>>>> mohit componentDidUpdate()");
-  //   }
-  // }, [number, number1]);
-
-  // useMemo(() => {
-  //   if (number !== 0) {
-  //     console.log(">>>>> mohit componentWillUpdate()");
-  //   }
-  // }, [number]);
-
-  useEffect(function () {
-    setNumber(number + 90);
-  }, []);
-
-  const increment = () => {
-    setNumber(number + 1);
+  const callBack = (number) => {
+    setInitvalue(number);
   };
 
-  // const decrement = () => {
-  //   setNumber(number - 1);
-  // };
-
   return (
-    <>
-      {console.log(">>>>> mohit render()")}
-      <div className="mt-5 mb-5" style={{ textAlign: "center" }}>
-        <CounterPrint number={number} />
-        <button onClick={increment} className="mt-4">
-          Incement
-        </button>
-      </div>
-    </>
+    <div className="mt-5 mb-5" style={{ textAlign: "center" }}>
+      <div>{initvalue}</div>
+      <CounterPrint value={initvalue} callBack={callBack} />
+    </div>
   );
 };
 
--- src/components/CounterPrint.jsx
+++ src/components/CounterPrint.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CounterPrint = (Props) => {
-  const { number } = Props;
-  return <div>{number}</div>;
+  const { value, callBack } = Props;
+  const [number, setNumber] = useState(value);
+
+  const increment = () => {
+    // set numner value
+    setNumber(number + 1);
+  };
+
+  useEffect(() => {
+    // Call back function and pass nunber state as argument
+    callBack(number);
+  }, [number]);
+
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 };
 
 export default CounterPrint;
